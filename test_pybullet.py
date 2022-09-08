1e6f1f1c1d2dea07f65580641ac3b751249f301f
Andre Lim <andrelimzs@outlook.com>
2022-09-08 12:41:19 +0800

    Clean up test_pybullet

--- test_pybullet.py
+++ test_pybullet.py
@@ -10,32 +10,39 @@ from pybullet_utils import bullet_client as bc
 import matplotlib.pyplot as plt
 
 def step(p, action):
-    x_threshold = 0.4
-    theta_threshold_radians = 12 * 2 * math.pi / 360
+    # Send torque to joints (setJointMotorControl2)
+    # Parameters:
+    #   objUid
+    #   jointIndex
+    #   controlMode
+    #   force
+    p.setJointMotorControl2(RRrobot, 0, p.TORQUE_CONTROL, force=action[0])
+    p.setJointMotorControl2(RRrobot, 1, p.TORQUE_CONTROL, force=action[1])
 
-    force = action[0]
-
-    p.setJointMotorControl2(cartpole, 0, p.TORQUE_CONTROL, force=force)
+    # Simulate one time step
     p.stepSimulation()
 
-    state = p.getJointState(cartpole, 1)[0:2] + p.getJointState(cartpole, 0)[0:2]
-    theta, theta_dot, x, x_dot = state
+    # Return state (getJointState)
+    # Parameters:
+    #   bodyID
+    #   jointIndex
+    # Returns:
+    #   jointPosition
+    #   jointvelocity
+    #   jointReactionForces
+    #   appliedJointMotorTroque (Only in VELOCITY_CONTROL or POSITION_CONTROL)
+    state = p.getJointState(RRrobot, 1)[0:2] + p.getJointState(RRrobot, 0)[0:2]
 
-    done =  x < -x_threshold \
-                or x > x_threshold \
-                or theta < -theta_threshold_radians \
-                or theta > theta_threshold_radians
-    done = bool(done)
-    reward = 1.0
-    return np.array(state), reward, done, {}
+    # Return state = { q1, q1_d, q2, q2_d }
+    return np.array(state)
 
 def render(p, physics_client_id):
     _render_width = 320
     _render_height = 200
 
     base_pos=[0,0,0]
-    _cam_dist = 2
-    _cam_pitch = 0.3
+    _cam_dist = 3
+    _cam_pitch = -90
     _cam_yaw = 0
     if (physics_client_id>=0):
         view_matrix = p.computeViewMatrixFromYawPitchRoll(
@@ -70,30 +77,34 @@ physics_client_id = p._client
 
 # Reset simulation and load model
 p.resetSimulation()
-cartpole = p.loadURDF(os.path.join(pybullet_data.getDataPath(), "cartpole.urdf"),
-                                 [0, 0, 0])
-p.changeDynamics(cartpole, -1, linearDamping=0, angularDamping=0)
-p.changeDynamics(cartpole, 0, linearDamping=0, angularDamping=0)
-p.changeDynamics(cartpole, 1, linearDamping=0, angularDamping=0)
-timeStep = 0.02
-p.setJointMotorControl2(cartpole, 1, p.VELOCITY_CONTROL, force=0)
-p.setJointMotorControl2(cartpole, 0, p.VELOCITY_CONTROL, force=0)
-p.setGravity(0, 0, -9.8)
+RRrobot = p.loadURDF("RR_planar_robot.urdf", [0, 0, 0])
+
+# Remove all damping
+p.changeDynamics(RRrobot, -1, linearDamping=0, angularDamping=0)
+p.changeDynamics(RRrobot, 0, linearDamping=0, angularDamping=0)
+p.changeDynamics(RRrobot, 1, linearDamping=0, angularDamping=0)
+
+# Disable joint motors to use torque control
+p.setJointMotorControl2(RRrobot, 0, p.VELOCITY_CONTROL, force=0)
+p.setJointMotorControl2(RRrobot, 1, p.VELOCITY_CONTROL, force=0)
+
+# Set gravity
+p.setGravity(0, 0, 0)
+
+# Set simulation time step and disable realtime
+timeStep = 0.01
 p.setTimeStep(timeStep)
 p.setRealTimeSimulation(0)
 
-randstate = np.random.uniform(low=-0.05, high=0.05, size=(4,))
-p.resetJointState(cartpole, 1, randstate[0], randstate[1])
-p.resetJointState(cartpole, 0, randstate[2], randstate[3])
+# Reset joint states
+p.resetJointState(RRrobot, 0, targetValue=0, targetVelocity=0)
+p.resetJointState(RRrobot, 1, targetValue=0, targetVelocity=0)
 
 
 # Random control
-for _ in range(1000):
-    action = [np.random.random() - 0.5, None]
+for i in range(1000):
+    # action = [10*(np.random.random() - 0.5), None]
+    action = [0, 0.1*math.sin(10*i/100)]
     step(p, action)
-    time.sleep(0.01)
+    rgb_array = render(p, physics_client_id)
 
-# Render in plt
-# rgb_array = render(p, physics_client_id)
-# plt.imshow(rgb_array)
-# plt.show()
